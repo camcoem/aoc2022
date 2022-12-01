7c48d65f59e8541e9eb084dffec1d80ffb422669
camcoem <94968301+camcoem@users.noreply.github.com>
2022-12-01 23:03:23 +0100

    solutions day 1

--- day01.mjs
+++ day01.mjs
@@ -1,18 +1,34 @@
 import { readFileSync } from "node:fs";
 
-const lines = readFileSync("day??.txt", { encoding: "utf-8" }) // read day??.txt content
+const elves = readFileSync("input1.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
   .split("\n") // Split on newline
   .map(Number); // Parse each line into a number
 
 function part1() {
-  //do something here
+  const calories = elves.map((elf) => {
+    const calories = elf.split("\n").map(Number);
+    return calories.reduce((previous, current) => previous + current, 0);
+  });
+
+  console.log(Math.max(...calories));
 }
 
 function part2() {
-  //do something here
+  const calories = elves.map((elf) => {
+    const calories = elf.split("\n").map(Number);
+    return calories.reduce((previous, current) => previous + current, 0);
+  });
+
+  calories.sort((a, b) => b - a);
+  // sort in descending order, so it is possible to get the first three elves with more snacks
+
+  console.log(
+    calories.slice(0, 3).reduce((previous, current) => previous + current, 0)
+  );
+  // slice from index 0, 3 positions and then again the sum, already created in the first part1
 }
 
-part1();
-part2();
+part1(68442);
+part2(204837);
